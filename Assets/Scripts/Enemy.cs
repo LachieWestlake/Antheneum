bc1dd05cd9465a641ac3836b3fb7ade1eea0c817
Jeremy <jeremy.v.maxwell@bigpond.com>
2019-02-10 22:55:45 +1100

    added health bars to enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+	public HealthBar healthBar;
     public float runSpeed;
     private Transform playerPosistion;
     private Player player;
 	private Rigidbody2D rb;
 	public float enemyDrag = 0.01f;
 	public int hitPoints = 20;
+	private int maxHealth;
 
 	public Transform PlayerPosistion
 	{
@@ -33,16 +35,22 @@ public class Enemy : MonoBehaviour {
 
 	public void Start ()
     {
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         playerPosistion = player.GetComponent<Transform>();
 		rb = GetComponent<Rigidbody2D>();
 		rb.drag = enemyDrag;
+		maxHealth = hitPoints;
     }
 	
 	public void Move ()
 	{
 		//Moves towards the player at the given speed
 	    transform.position = Vector2.MoveTowards(transform.position, playerPosistion.position, runSpeed * Time.deltaTime);
+	}
+
+	public void UpdateHealth()
+	{
+		healthBar.SetSize((float)hitPoints/maxHealth);
 	}
 
 	//If the enemy colides with a projectile
@@ -55,6 +63,8 @@ public class Enemy : MonoBehaviour {
 			hitPoints -= 10;
 			if (hitPoints <= 0)
 				Destroy(gameObject);
+
+			UpdateHealth();
         }
     }
 }
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+	private Transform bar;
+
+	// Use this for initialization
+	void Start () {
+		bar = transform.Find("Bar");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetSize(float sizeNormalised)
+	{
+		bar.localScale = new Vector3(sizeNormalised, 1f);
+	}
+}

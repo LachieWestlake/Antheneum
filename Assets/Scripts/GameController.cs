8386f149c7ff7351d41587ef302357df18688b69
Jeremy <jeremy.v.maxwell@bigpond.com>
2019-02-11 17:29:09 +1100

    Added background colour and GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController instance { get; private set; }
+	public Player player;
+	private GameObject[] arrayOfEnemies;
+	public Text enemyAmount;
+
+	void Awake()
+	{
+		if (instance != null) throw new System.Exception();
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		arrayOfEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+		//Writes the amount of enemies to the screen
+		enemyAmount.text = "Enemies: " + arrayOfEnemies.Length;
+	}
+}
